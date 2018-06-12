1d1b50a8516678a9391a96ed8dee680436353533
Chandra Gopalaiah <c.gopalaiah@samsung.com>
2018-06-11 17:03:18 -0700

    Added Problem_152

--- README.md
+++ README.md
@@ -35,6 +35,7 @@
 |139|[Word Break](https://leetcode.com/problems/word-break/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_139.java)|O(n^3) or O(n^2)|O(n)|Medium| DP, BFS |FB
 |146|[LRU Cache](https://leetcode.com/problems/lru-cache/) | [Solution](./problems/src/main/java/com/chandra/problems/Problem_146.java)|amortized O(1) | O(k) | Hard| Doubly Linked List, HashMap| FB
 |151|[Reverse Words in a String](https://leetcode.com/problems/reverse-words-in-a-string/) | [Solution](./problems/src/main/java/com/chandra/problems/Problem_151.java)|O(n) | O(n) | Medium| |
+|152|[Maximum Product Subarray](https://leetcode.com/problems/maximum-product-subarray/) | [Solution](./problems/src/main/java/com/chandra/problems/Problem_152.java)|O(n) | O(1) | Medium| | FB
 |*161|[One Edit Distance](https://leetcode.com/problems/one-edit-distance/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_161.java)|O(n) | O(1) | Easy| | FB
 |167|[Two Sum II - Input array is sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_167.java)|O(n) | O(1) | Easy| |
 |*170|[Two Sum III - Data structure design](https://leetcode.com/problems/two-sum-iii-data-structure-design/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_170.java)|O(n) | O(n) | Easy| HashTable |
---
+++ problems/src/main/java/com/chandra/problems/Problem_152.java
@@ -0,0 +1,59 @@
+package com.chandra.problems;
+
+/**
+ * 152. Maximum Product Subarray
+ *
+ * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+ Example 1:
+
+ Input: [2,3,-2,4]
+ Output: 6
+ Explanation: [2,3] has the largest product 6.
+ Example 2:
+
+ Input: [-2,0,-1]
+ Output: 0
+ Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+ */
+
+public class Problem_152 {
+    public static class Solution_1 {
+        public int maxProduct(int[] nums) {
+            /*
+            We know if we multiply two numbers, the product will be big if the numbers are both big in magnitude and are of same sign.
+
+Two large negative numbers produce a large positive product
+Two large positive numbers produce a large positive product
+A large positive and a large negative number produce a large negative product (which may or may not be used further with another negative number to produce a large positive product)
+Thus, to find the maximum product in a subarray, we have to keep track of both local minimum product as well as local maximum product.
+             */
+            int localMax = nums[0];
+            int localMin = nums[0];
+            int maxProd = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < 0) {
+                    // we swap localMin and localMax if current number is less than zero
+                    // since localmin can be negative and multiplying by negative number would yield bigger number
+                    int temp = localMax;
+                    localMax = localMin;
+                    localMin = temp;
+                }
+
+                localMax = Math.max(nums[i], localMax * nums[i]);
+                localMin = Math.min(nums[i], localMin * nums[i]);
+
+                maxProd = Math.max(localMax, maxProd);
+            }
+
+            return maxProd;
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem_152.Solution_1 solution_1 = new Solution_1();
+        System.out.println(solution_1.maxProduct(new int[]{2,3,-2,4}));
+    }
+}
