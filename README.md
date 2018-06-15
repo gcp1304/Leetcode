8fa1301d2daab0ae5e7bc0bbc10c43c77ab66912
Chandra Gopalaiah <c.gopalaiah@samsung.com>
2018-06-15 16:33:51 -0700

    Added Problem_78

--- README.md
+++ README.md
@@ -33,6 +33,7 @@
 |49|[Group Anagrams](https://leetcode.com/problems/group-anagrams/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_49.java)|O(m*klogk)|O(mk)|Medium|HashMap|FB
 |50|[Pow(x, n)](https://leetcode.com/problems/powx-n/description/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_50.java)|O(logn)|O(logn)|Medium||FB
 |77|[Combinations](https://leetcode.com/problems/combinations/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_77.java)|O(n!) |O(n)||Medium|Backtracking||
+|78|[Subsets](https://leetcode.com/problems/subsets/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_78.java)|O(n^2) |O(1)|Medium|Backtracking|
 |102|[Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_102.java)|O(n| O(h) | Medium |BFS | FB
 |121|[Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_121.java)|O(n)|O(1)|Easy| Array |FB
 |125|[Valid Palindrome](https://leetcode.com/problems/valid-palindrome/)|[Solution](./problems/src/main/java/com/chandra/problems/Problem_125.java)|O(n)|O(1)|Easy| Two Pointers|FB
---
+++ problems/src/main/java/com/chandra/problems/Problem_78.java
@@ -0,0 +1,47 @@
+package com.chandra.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 78. Subsets
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+ Note: The solution set must not contain duplicate subsets.
+
+ Example:
+
+ Input: nums = [1,2,3]
+ Output:
+ [
+ [3],
+ [1],
+ [2],
+ [1,2,3],
+ [1,3],
+ [2,3],
+ [1,2],
+ []
+ ]
+ */
+public class Problem_78 {
+    public static class Solution_1 {
+        //This problem asks us to output all subsets. Every time we add a new valid element into subset,
+        // it forms a valid 'subset'. This means that we have to add the subset list in every recursion.
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            backtracking(res, nums, new ArrayList<>(), 0);
+            return res;
+        }
+
+        private void backtracking(List<List<Integer>> res, int[] nums, List<Integer> curSubset, int start) {
+            res.add(new ArrayList<>(curSubset));
+            for (int i = start; i < nums.length; i++) {
+                curSubset.add(nums[i]);
+                backtracking(res, nums, curSubset, i + 1);
+                curSubset.remove(curSubset.size() - 1);
+            }
+        }
+    }
+}
