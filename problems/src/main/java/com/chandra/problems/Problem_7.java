db75f2719854401e7cc6110bc99ee1faf4eb8274
Chandra Gopalaiah <c.gopalaiah@samsung.com>
2018-07-13 08:22:22 -0700

    Modified Problem_7

--- problems/src/main/java/com/chandra/problems/Problem_7.java
+++ problems/src/main/java/com/chandra/problems/Problem_7.java
@@ -27,8 +27,8 @@ public class Problem_7 {
     public static class Solution_1 {
         public int reverse(int x) {
             int res = 0;
-            if (Math.abs(res) > Integer.MAX_VALUE/10) return 0;
             while (x != 0) {
+                if (Math.abs(res) > Integer.MAX_VALUE/10) return 0;
                 res = res * 10 + x % 10;
                 x /= 10;
             }
